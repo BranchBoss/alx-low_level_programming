0b4a7b24ef39a3ba8d134d1af860d1fcf2fbeea9
branchboss <my email>
2023-05-21 09:25:39 -0700

    helloworld

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,10 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
-
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
-	printf("\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
