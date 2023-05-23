9cb8206ab1f9c6b2e60a45a6d9ab1b7c122f817c
branchboss <my email>
2023-05-23 11:51:25 -0700

    3loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Description: Function that adds two integers and returns the result
+ * @a: input integer
+ * @b: input integer
+ * Return: Result to add a and b
+ */
+int add(int a, int b)
+{
+		return (a + b);
+}
